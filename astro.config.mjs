7ac001eea50096c70628aaf4ab6e54cfe0e40f5c
acemavrick <116685696+acemavrick@users.noreply.github.com>
2025-08-10 14:27:47 -0500

    Fixed nav links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://acemavrick.github.io/astro-tutorial',
-    base: '/astro-tutorial'
+    site: 'https://acemavrick.github.io',
+    base: '/astro-tutorial',
+    integrations: [sitemap()]
 });
